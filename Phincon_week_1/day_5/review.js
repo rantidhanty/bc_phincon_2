5a080494c08492ac1481ba4585fd7b28ff3e7513
Rantidhanty <rantidhanty@gmail.com>
2024-09-13 21:30:25 +0700

    add day 5

---
+++ Phincon_week_1/day_5/review.js
@@ -0,0 +1,13 @@
+/*
+Review materi 26 soal basic javascript
+*/
+
+// function hitungRataRata(nums){
+//     let rata = 0;
+//     for(let i = 0; i < nums.length; i++){
+//         rata += nums[i];
+//     }
+//     let hasil = rata / nums.length;
+//     return hasil
+// }
+// console.log(hitungRataRata([1, 2, 3, 4, 5]));
